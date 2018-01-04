94c57b5dd89326a96479f2bdac7a74cf0c94a418
Alberto <phil03@email.it>
2018-01-04 22:40:12 +0100

    aggiunta colonna quantità

--- resources/views/main/listaArticoli.blade.php
+++ resources/views/main/listaArticoli.blade.php
@@ -1,22 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Porta e offri</title>
+    <meta charset="utf-8">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 
+</head>
+<body>
 <div class="col-md-12">
 
-    <table class="table table-striped  table-condensed">
+    <table class="table table-bordered">
         <thead>
             <tr>
-                <th class="col-md-12 text-center"><small>Articoli</small></th>
+                <th class="col-md-10 text-center">Articoli</th>
+                <th class="col-md-2 text-center">Quantità</th>
             </tr>
         </thead>
         <tbody>
             @foreach($articoli as $articolo)
             <tr>
-                 <td>{!! $articolo->articolo !!}</td>
+                <td>{!! $articolo->articolo !!}</td>
+                <td></td>
             </tr>
             @endforeach
         </tbody>
     </table>
 
 </div>
+
+</body>
+</html>
+
 
 
 
